067d62d412484dad2fe6b0561aebbbd0580f00d2
skifast <skifastnotlast@gmail.com>
2022-07-21 23:41:03 -0600

    Some small additions and changes
    User can now see exercises they've added to their workout. Reps/distance soon to come.

---
+++ RenderPanels.java
@@ -0,0 +1,134 @@
+package workoutCalendar;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class RenderPanels {
+	private int partialWidth = 600;
+	private int partialHeight = 575; 
+	private int fullWidth = 1600;
+	private int fullHeight = 1000;
+	private int width;
+	private int height; 
+	private int borderThickness = 4; 
+	
+	private Color barbiePink = new Color(233, 65, 150);
+	private Color lightPink = new Color(250, 209, 230);
+	private Color powderblue = new Color(168, 234, 255);
+	
+	private List<JLabel> displayLabels;
+	
+	Font copperplateH1 = new Font("Copperplate", Font.PLAIN, 54);
+	Font copperplateH2 = new Font("Copperplate", Font.PLAIN, 24);
+	
+	Container c; 
+	
+	public RenderPanels() {
+		width = partialWidth;
+		height = partialHeight;
+	}
+	
+	public JPanel[] renderBasePanels(Container c) {
+		int borderThickness = 4; 
+		JPanel[] panels = new JPanel[9];
+		
+		JPanel userPanel = new JPanel(); 
+        userPanel.setBackground(Color.white);
+        userPanel.setPreferredSize(new Dimension(width, height));
+        userPanel.setBorder(BorderFactory.createMatteBorder(borderThickness, borderThickness, borderThickness, borderThickness, barbiePink));
+        c.add(userPanel);
+        panels[0] = userPanel;
+        
+        JPanel upperPanel = new JPanel(); 
+        upperPanel.setBackground(lightPink);
+        upperPanel.setPreferredSize(new Dimension(width - borderThickness*4, height/2 - borderThickness*6));
+        upperPanel.setBorder(BorderFactory.createMatteBorder(borderThickness, borderThickness, 0, borderThickness, Color.white));
+        userPanel.add(upperPanel);
+        userPanel.setLayout(new GridLayout(2, 1));
+        panels[1] = upperPanel;
+        
+        JPanel lowerPanel = new JPanel(); 
+        lowerPanel.setBackground(Color.white);
+        lowerPanel.setPreferredSize(new Dimension(width - borderThickness * 4, height/2 - borderThickness * 4));
+        lowerPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.white));
+        userPanel.add(lowerPanel);
+        panels[2] = lowerPanel;
+        
+        
+        JPanel lowerLeft = new JPanel();
+        lowerLeft.setBackground(lightPink);
+        lowerLeft.setPreferredSize(new Dimension(width/2 - borderThickness*4, height/2 - borderThickness*6));
+        lowerLeft.setBorder(BorderFactory.createMatteBorder(0, 0, borderThickness, borderThickness/2, Color.white));
+        lowerPanel.add(lowerLeft);
+        panels[3] = lowerLeft;
+        
+        JPanel lowerRight = new JPanel(); 
+        lowerRight.setBackground(lightPink);
+        lowerRight.setPreferredSize(new Dimension(width/2 - borderThickness*4, height/2 - borderThickness*6));
+        lowerRight.setBorder(BorderFactory.createMatteBorder(0, borderThickness/2, borderThickness, 0, Color.white));
+        lowerPanel.add(lowerRight);
+        panels[4] = lowerRight;
+        
+        return panels;
+	}
+	
+	public JPanel[] renderSecondRoundPanels(JPanel upperPanel, String number, JPanel[] panels) {
+		JPanel titlePanel = new JPanel(); 
+		titlePanel.setBackground(Color.white);
+		titlePanel.setPreferredSize(new Dimension(width - borderThickness * 8, 55));
+		//titlePanel.setLayout(new GridLayout(2, 1));
+		upperPanel.add(titlePanel);
+		panels[5] = titlePanel;
+		
+		JLabel title = new JLabel();
+		title.setText("Valerie's Workout");
+		title.setBounds(width/2 - title.getWidth()/2, 20, title.getWidth(), title.getHeight());
+		title.setFont(copperplateH1);
+		title.setForeground(barbiePink);
+		titlePanel.add(title);
+	
+		
+		JPanel datePanel = new JPanel(); 
+		datePanel.setBackground(powderblue);
+		datePanel.setPreferredSize(new Dimension(width/2, 30));
+		datePanel.setLayout(new FlowLayout());
+		upperPanel.add(datePanel);
+		panels[6] = datePanel;
+		
+		JLabel date = new JLabel(); 
+		date.setText("July " + number);
+		date.setBounds(width/2 - title.getWidth()/2, 50, title.getWidth(), title.getHeight());
+		date.setFont(copperplateH2);
+		date.setForeground(Color.white);
+		datePanel.add(date);
+		
+		 
+		JPanel exerciseCategoryPanel = new JPanel(); 
+		exerciseCategoryPanel.setBackground(powderblue);
+		exerciseCategoryPanel.setPreferredSize(new Dimension(width/2, 30));
+		exerciseCategoryPanel.setLayout(new FlowLayout());
+		upperPanel.add(exerciseCategoryPanel);
+		panels[7] = exerciseCategoryPanel; 
+		
+		JPanel exercisesPanel = new JPanel(); 
+		exercisesPanel.setBackground(Color.white);
+		exercisesPanel.setPreferredSize(new Dimension(width - borderThickness * 8, 125));
+		exercisesPanel.setLayout(new GridLayout(displayLabels.size(), 1));
+		//exercisesPanel.setBounds(50, 50, 100, 100);
+		//exercisesPanel.setPreferredSize(new Dimension(width - borderThickness * 8, 75));
+		upperPanel.add(exercisesPanel);
+		panels[8] = exercisesPanel; 
+		
+		return panels;
+	}
+	
+}
--- userInput.java
+++ userInput.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -29,14 +31,22 @@ public class userInput extends JFrame{
 	private int fullHeight = 1000;
 	private int width;
 	private int height; 
+	private int borderThickness = 4; 
 	
 	private Color barbiePink = new Color(233, 65, 150);
 	private Color lightPink = new Color(250, 209, 230);
 	private Color powderblue = new Color(168, 234, 255);
 	
+	private List<JLabel> displayLabels;
+	
+	Font copperplateH1 = new Font("Copperplate", Font.PLAIN, 54);
+	Font copperplateH2 = new Font("Copperplate", Font.PLAIN, 24);
+	
 	public userInput() {
 		width = partialWidth;
 		height = partialHeight;
+		
+		displayLabels = new ArrayList<>(); 
 		  
         setTitle("Record Your Workout");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,127 +65,107 @@ public class userInput extends JFrame{
 	}
 	
 	private void assembleWindowPanes(Container c, String number) {
-		int borderThickness = 4; 
-		JPanel[] panels = renderBasePanels(c); 
-		JPanel lowerLeft = panels[3];
+		RenderPanels renderPanels = new RenderPanels(); 
+		
+		JPanel[] panels = renderPanels.renderBasePanels(c); 
 		JPanel upperPanel = panels[1];
-		Font copperplateH1 = new Font("Copperplate", Font.PLAIN, 54);
-		Font copperplateH2 = new Font("Copperplate", Font.PLAIN, 24);
-		
-		
-		JPanel titlePanel = new JPanel(); 
-		titlePanel.setBackground(Color.white);
-		titlePanel.setPreferredSize(new Dimension(width - borderThickness * 8, 55));
-		//titlePanel.setLayout(new GridLayout(2, 1));
-		upperPanel.add(titlePanel);
-		
-		JLabel title = new JLabel();
-		title.setText("Valerie's Workout");
-		title.setBounds(width/2 - title.getWidth()/2, 20, title.getWidth(), title.getHeight());
-		title.setFont(copperplateH1);
-		title.setForeground(barbiePink);
-		titlePanel.add(title);
-		
-		
-		JPanel datePanel = new JPanel(); 
-		datePanel.setBackground(powderblue);
-		datePanel.setPreferredSize(new Dimension(width/2, 30));
-		datePanel.setLayout(new FlowLayout());
-		upperPanel.add(datePanel);
-		
-		
-		JLabel date = new JLabel(); 
-		date.setText("July " + number);
-		date.setBounds(width/2 - title.getWidth()/2, 50, title.getWidth(), title.getHeight());
-		date.setFont(copperplateH2);
-		date.setForeground(Color.white);
-		datePanel.add(date);
-		
-		 
-		JPanel exerciseCategoryPanel = new JPanel(); 
-		exerciseCategoryPanel.setBackground(powderblue);
-		exerciseCategoryPanel.setPreferredSize(new Dimension(width/2, 30));
-		exerciseCategoryPanel.setLayout(new FlowLayout());
-		upperPanel.add(exerciseCategoryPanel);
+		JPanel lowerLeft = panels[3];
+		panels = renderPanels.renderSecondRoundPanels(upperPanel, number, panels); 
+		JPanel exerciseCategoryPanel = panels[7];
+		JPanel exercisesPanel = panels[8];
 		
 		JLabel exerciseCategories = new JLabel();
-		exerciseCategories.setBounds(width/2 - title.getWidth()/2, 200, title.getWidth(), title.getHeight());
+		exerciseCategories.setBounds(width/2 - 100, 200, 400, 20);
 		exerciseCategories.setFont(copperplateH2);
 		exerciseCategories.setForeground(Color.white);
 		exerciseCategoryPanel.add(exerciseCategories);
 		
-		JPanel exercisesPanel = new JPanel(); 
-		exercisesPanel.setBackground(Color.white);
-		exercisesPanel.setPreferredSize(new Dimension(width - borderThickness * 8, 125));
-		//exercisesPanel.setBounds(50, 50, 100, 100);
-		//exercisesPanel.setPreferredSize(new Dimension(width - borderThickness * 8, 75));
-		upperPanel.add(exercisesPanel);
-        
+		
+		List<JButton> buttons = new ArrayList<>();
+		List<JComboBox<String>> selectors = new ArrayList<>(); 
+		
         JButton selectType = new JButton();
         renderButton(selectType, "Submit Exercise Type", true, lowerLeft);
+        buttons.add(selectType);
         
         JButton selectStrength = new JButton();
         renderButton(selectStrength, "Submit Strength Type", false, lowerLeft);
+        buttons.add(selectStrength);
         
         JButton selectCardio = new JButton();
         renderButton(selectCardio, "Submit Cardio Type", false, lowerLeft); 
+        buttons.add(selectCardio);
         
         //by muscle group
         JButton selectShoulder = new JButton(); 
         renderButton(selectShoulder, "Select Shoulder Exercise", false, lowerLeft); 
+        buttons.add(selectShoulder);
         
         JButton selectBack = new JButton(); 
         renderButton(selectBack, "Select Back Exercise", false, lowerLeft); 
+        buttons.add(selectBack);
         
         JButton selectQuads = new JButton(); 
         renderButton(selectQuads, "Select Quad Exercise", false, lowerLeft); 
+        buttons.add(selectQuads);
         
         JButton selectHamstrings = new JButton(); 
         renderButton(selectHamstrings, "Select Hamstrings Exercise", false, lowerLeft);
+        buttons.add(selectHamstrings);
         
         JButton selectBiceps = new JButton(); 
         renderButton(selectBiceps, "Select Bicep Exercise", false, lowerLeft);
+        buttons.add(selectBiceps);
         
         JButton selectGlutes = new JButton(); 
         renderButton(selectGlutes, "Select Glute Exercise", false, lowerLeft);
-        
+        buttons.add(selectGlutes);
         
        //drop down boxes 
         String[] exerciseGroup = {"Strength", "Cardio", "Stretching"};
         JComboBox<String> exerciseType = new JComboBox<>(exerciseGroup);
         renderComboBox(exerciseType, true, lowerLeft); 
+        selectors.add(exerciseType);
         
         String[] strengthCategories = getStrengthCategories();
         JComboBox<String> strengthType = new JComboBox<>(strengthCategories);
         renderComboBox(strengthType, false, lowerLeft);
+        selectors.add(strengthType);
         
         String[] cardioCategories = getCardioCategories();
         JComboBox<String> cardioType = new JComboBox<>(cardioCategories);
         renderComboBox(cardioType, false, lowerLeft);
+        selectors.add(cardioType);
         
         String[] shoulderCategories = getShoulderCategories();
         JComboBox<String> shoulderType = new JComboBox<>(shoulderCategories);
         renderComboBox(shoulderType, false, lowerLeft);
+        selectors.add(shoulderType);
         
         String[] backCategories = getBackCategories();
         JComboBox<String> backType = new JComboBox<>(backCategories);
         renderComboBox(backType, false, lowerLeft);
+        selectors.add(backType);
         
-        String[] quadsCategories = getShoulderCategories();
+        String[] quadsCategories = getQuadCategories();
         JComboBox<String> quadsType = new JComboBox<>(quadsCategories);
         renderComboBox(quadsType, false, lowerLeft);
+        selectors.add(quadsType);
         
-        String[] hamstringCategories = getShoulderCategories();
+        String[] hamstringCategories = getHamstringCategories();
         JComboBox<String> hamstringType = new JComboBox<>(hamstringCategories);
         renderComboBox(hamstringType, false, lowerLeft);
+        selectors.add(hamstringType);
         
-        String[] bicepCategories = getShoulderCategories();
+        String[] bicepCategories = getBicepCategories();
         JComboBox<String> bicepType = new JComboBox<>(bicepCategories);
         renderComboBox(bicepType, false, lowerLeft);
+        selectors.add(bicepType);
         
-        String[] gluteCategories = getShoulderCategories();
+        String[] gluteCategories = getGluteCategories();
         JComboBox<String> gluteType = new JComboBox<>(gluteCategories);
         renderComboBox(gluteType, false, lowerLeft);
+        selectors.add(gluteType);
         
         selectType.addActionListener(new ActionListener() {
 			@Override
@@ -200,62 +190,122 @@ public class userInput extends JFrame{
 			}
 		});
         
-        selectStrength.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				selectStrength.setVisible(false);
-				strengthType.setVisible(false);
-				
-				String type = (String) strengthType.getSelectedItem();
-				if(type.equals("Shoulders")) {
-					shoulderType.setVisible(true);
-					selectShoulder.setVisible(true);
-					lowerLeft.add(selectShoulder);
-					lowerLeft.add(shoulderType);
-				}
-				else if(type.equals("Back")) {
-					lowerLeft.add(selectBack);
-					lowerLeft.add(backType);					
-				}
-				else if(type.equals("Quads")) {
-					lowerLeft.add(selectQuads);
-					lowerLeft.add(quadsType);					
-				}
-				else if(type.equals("Hamstrings")) {
-					lowerLeft.add(selectHamstrings);
-					lowerLeft.add(hamstringType);					
-				}
-				else if(type.equals("Biceps")){
-					lowerLeft.add(selectBiceps);
-					lowerLeft.add(bicepType);
-					
-				}
-				else if(type.equals("Glutes")) {
-					lowerLeft.add(selectGlutes);
-					lowerLeft.add(gluteType);					
-				}
-						
-			}
-		});
+        strengthSelectorActionListeners(buttons, selectors, lowerLeft);
+        
+        
         
         
         selectShoulder.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("heeelp");
 				
 				shoulderType.setVisible(false);
 				selectShoulder.setVisible(false);
 				
 				String type = (String) shoulderType.getSelectedItem();
 				JLabel shoulder = new JLabel(type);
-				exercisesPanel.add(shoulder);
+				displayLabels.add(shoulder);
+				updateDisplayLabels(exercisesPanel);
 
-				
-				lowerLeft.add(exerciseType);
 				lowerLeft.add(selectType);
-				exerciseType.setVisible(true);
-				selectType.setVisible(true);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
+			}
+		});
+        
+        selectBack.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				backType.setVisible(false);
+				selectBack.setVisible(false);
+				
+				String type = (String) backType.getSelectedItem();
+				JLabel back = new JLabel(type);
+				displayLabels.add(back);
+				updateDisplayLabels(exercisesPanel);
+
+				lowerLeft.add(selectType);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
+			}
+		});
+        
+        selectQuads.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				quadsType.setVisible(false);
+				selectQuads.setVisible(false);
+				
+				String type = (String) quadsType.getSelectedItem();
+				JLabel quads = new JLabel(type);
+				displayLabels.add(quads);
+				updateDisplayLabels(exercisesPanel);
+
+				lowerLeft.add(selectType);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
+			}
+		});
+        
+        selectHamstrings.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				hamstringType.setVisible(false);
+				selectHamstrings.setVisible(false);
+				
+				String type = (String) hamstringType.getSelectedItem();
+				JLabel hamstrings = new JLabel(type);
+				displayLabels.add(hamstrings);
+				updateDisplayLabels(exercisesPanel);
+
+				lowerLeft.add(selectType);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
+			}
+		});
+        
+        selectBiceps.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				bicepType.setVisible(false);
+				selectBiceps.setVisible(false);
+				
+				String type = (String) bicepType.getSelectedItem();
+				JLabel biceps = new JLabel(type);
+				displayLabels.add(biceps);
+				updateDisplayLabels(exercisesPanel);
+
+				lowerLeft.add(selectType);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
+			}
+		});
+        
+        selectGlutes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				gluteType.setVisible(false);
+				selectGlutes.setVisible(false);
+				
+				String type = (String) gluteType.getSelectedItem();
+				JLabel glutes = new JLabel(type);
+				displayLabels.add(glutes);
+				updateDisplayLabels(exercisesPanel);
+
+				lowerLeft.add(selectType);
+				lowerLeft.add(exerciseType);
+				strengthType.setVisible(true);
+				selectStrength.setVisible(true);
 			}
 		});
         
@@ -272,69 +322,33 @@ public class userInput extends JFrame{
 				}
 			}
 		});
-        
-        
-        /*
-        
-        String[] stretchingCategories = {}; */
-       
-        
-        /*
-        JButton next = new JButton();
-        next.setText("Next");
-        next.setForeground(barbiePink);
-        next.setBackground(powderblue);
-        next.setPreferredSize(new Dimension(200, 50));
-        //set border to empty
-        next.setBorder(emptyBorder);
-        next.setOpaque(true);
-        lowerRight.add(next);*/
        
 	}
 	
-	private JPanel[] renderBasePanels(Container c) {
-		int borderThickness = 4; 
-		JPanel[] panels = new JPanel[5];
+	private void updateDisplayLabels(JPanel exercisesPanel) {
+		//exercisesPanel.add(label);
+		//exercisesPanel = new JPanel(); 
+		/*exercisesPanel.setBackground(Color.red);
+		exercisesPanel.setPreferredSize(new Dimension(width - borderThickness * 8, 125));
+		upperPanel.add(exercisesPanel);
+	
+		exercisesPanel.setLayout(new GridLayout(displayLabels.size(), 1));
+		*/
+		if(displayLabels.size() < 2) {
+			exercisesPanel.setLayout(new GridLayout(displayLabels.size(), 1));
+		}
+		else if(displayLabels.size() > 2) {
+			exercisesPanel.setLayout(new GridLayout(displayLabels.size() / 2, 2));
+		}
+		else if(displayLabels.size() > 6) {
+			exercisesPanel.setLayout(new GridLayout(displayLabels.size() / 3, 3));
+		}
 		
-		JPanel userPanel = new JPanel(); 
-        userPanel.setBackground(Color.white);
-        userPanel.setPreferredSize(new Dimension(width, height));
-        userPanel.setBorder(BorderFactory.createMatteBorder(borderThickness, borderThickness, borderThickness, borderThickness, barbiePink));
-        c.add(userPanel);
-        panels[0] = userPanel;
-        
-        JPanel upperPanel = new JPanel(); 
-        upperPanel.setBackground(lightPink);
-        upperPanel.setPreferredSize(new Dimension(width - borderThickness*4, height/2 - borderThickness*6));
-        upperPanel.setBorder(BorderFactory.createMatteBorder(borderThickness, borderThickness, 0, borderThickness, Color.white));
-        userPanel.add(upperPanel);
-        userPanel.setLayout(new GridLayout(2, 1));
-        panels[1] = upperPanel;
-        
-        JPanel lowerPanel = new JPanel(); 
-        lowerPanel.setBackground(Color.white);
-        lowerPanel.setPreferredSize(new Dimension(width - borderThickness * 4, height/2 - borderThickness * 4));
-        lowerPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.white));
-        userPanel.add(lowerPanel);
-        panels[2] = lowerPanel;
-        
-        
-        JPanel lowerLeft = new JPanel();
-        lowerLeft.setBackground(lightPink);
-        lowerLeft.setPreferredSize(new Dimension(width/2 - borderThickness*4, height/2 - borderThickness*6));
-        lowerLeft.setBorder(BorderFactory.createMatteBorder(0, 0, borderThickness, borderThickness/2, Color.white));
-        lowerPanel.add(lowerLeft);
-        panels[3] = lowerLeft;
-        
-        JPanel lowerRight = new JPanel(); 
-        lowerRight.setBackground(lightPink);
-        lowerRight.setPreferredSize(new Dimension(width/2 - borderThickness*4, height/2 - borderThickness*6));
-        lowerRight.setBorder(BorderFactory.createMatteBorder(0, borderThickness/2, borderThickness, 0, Color.white));
-        lowerPanel.add(lowerRight);
-        panels[4] = lowerRight;
-        
-        return panels;
+		for(int i = 0; i < displayLabels.size(); i++) {
+			exercisesPanel.add(displayLabels.get(i));
+		}
 	}
+	
 	
 	private String[] getStrengthCategories() {
 		String[] strengthCategories = {"Shoulders", "Back", "Quads", "Hamstrings", "Biceps", "Glutes"};
@@ -397,5 +411,55 @@ public class userInput extends JFrame{
 		comboBox.setPreferredSize(new Dimension(200, 50));
 		if(addToPage)
 			addToPanel.add(comboBox);
+	}
+	
+	private void strengthSelectorActionListeners(List<JButton> buttons, List<JComboBox<String>> selectors, JPanel lowerLeft){
+		buttons.get(1).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				buttons.get(1).setVisible(false);
+				selectors.get(1).setVisible(false);
+				
+				String type = (String) selectors.get(1).getSelectedItem();
+				if(type.equals("Shoulders")) {
+					selectors.get(3).setVisible(true);
+					buttons.get(3).setVisible(true);
+					lowerLeft.add(buttons.get(3));
+					lowerLeft.add(selectors.get(3));
+				}
+				else if(type.equals("Back")) {
+					selectors.get(4).setVisible(true);
+					buttons.get(4).setVisible(true);
+					lowerLeft.add(buttons.get(4));
+					lowerLeft.add(selectors.get(4));					
+				}
+				else if(type.equals("Quads")) {
+					selectors.get(5).setVisible(true);
+					buttons.get(5).setVisible(true);
+					lowerLeft.add(buttons.get(5));
+					lowerLeft.add(selectors.get(5));					
+				}
+				else if(type.equals("Hamstrings")) {
+					selectors.get(6).setVisible(true);
+					buttons.get(6).setVisible(true);
+					lowerLeft.add(buttons.get(6));
+					lowerLeft.add(selectors.get(6));					
+				}
+				else if(type.equals("Biceps")){
+					selectors.get(7).setVisible(true);
+					buttons.get(7).setVisible(true);
+					lowerLeft.add(buttons.get(7));
+					lowerLeft.add(selectors.get(7));
+					
+				}
+				else if(type.equals("Glutes")) {
+					selectors.get(8).setVisible(true);
+					buttons.get(8).setVisible(true);
+					lowerLeft.add(buttons.get(8));
+					lowerLeft.add(selectors.get(8));					
+				}
+						
+			}
+		});
 	}
 }
